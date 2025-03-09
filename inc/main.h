8f9e55dd14919cc97b9cfcf857121ef1b88f430f
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-03-09 14:58:35 +0900

    WIP

--- inc/main.h
+++ inc/main.h
@@ -6,25 +6,42 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 11:47:22 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/03/09 13:55:45 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/03/09 14:53:45 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAIN_H
 # define MAIN_H
 
+# define MAX_IMG_COUNT 64
+
 # include "libft.h"
 # include "mlx.h"
 
 typedef struct s_glx	t_glx;
+typedef struct s_img	t_img;
+
 
 typedef struct s_glx
 {
 	void				*mlx;
+	void				*win;
+	t_img				*img;
+	void				(*run)(t_glx *self);
 	void				(*free)(t_glx *self);
 }						t_glx;
 
-t_glx					*glx_init(void);
+typedef struct s_img
+{
+	void	*img[64];
+	int		w;
+	int		h;
+	int		count;
+}	t_img;
+
+
+t_glx					*glx_init(char *title, int win_x, int win_y);
+void					glx_run(t_glx *self);
 void					glx_free(t_glx *self);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,24 +6,45 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 11:46:33 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/03/09 13:55:41 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/03/09 14:56:52 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-t_glx	*glx_init(void)
+t_glx	*glx_init(char *title, int win_x, int win_y)
 {
 	t_glx	*glx;
 
 	glx = ft_calloc(1, sizeof(t_glx));
 	glx->mlx = mlx_init();
+	glx->win = mlx_new_window(glx->mlx, win_x, win_y, title);
 	glx->free = glx_free;
+	glx->run = glx_run;
 	return (glx);
+}
+
+void	glx_load_img(t_glx *self, char *img_path)
+{
+	int		*w;
+	int		*h;
+	void	*(*load)(void *, char *, int *, int *);
+
+	load = mlx_xpm_file_to_image;
+	if (self->img->count >= MAX_IMG_COUNT)
+		return ;
+	w = &(self->img->w);
+	h = &(self->img->h);
+	self->img->img[self->img->count++] = load(self->mlx, img_path, w, h);
 }
 
 void	glx_free(t_glx *self)
 {
 	free(self->mlx);
 	free(self);
+}
+
+void	glx_run(t_glx *self)
+{
+	mlx_loop(self->mlx);
 }
