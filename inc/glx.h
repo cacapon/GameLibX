bb0b6eafa2c6cef998df1399906351c59be3aee0
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-03-11 12:38:11 +0900

    winを追加

--- inc/glx.h
+++ inc/glx.h
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 11:47:22 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/03/11 12:31:15 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/03/11 12:37:16 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,12 @@
 typedef struct s_glx
 {
 	void	*mlx;
+	void	*win;
 	void	(*free)(struct s_glx *self);
 	void	(*run)(struct s_glx *self);
 }			t_glx;
 
-t_glx		*glx_init(void);
+t_glx		*glx_init(char *title, int win_w, int win_h);
 void		glx_free(t_glx *self);
 void		glx_run(t_glx *self);
 
--- src/glx.c
+++ src/glx.c
@@ -6,18 +6,19 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 11:46:33 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/03/11 12:32:16 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/03/11 12:36:42 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "glx.h"
 
-t_glx	*glx_init(void)
+t_glx	*glx_init(char *title, int win_w, int win_h)
 {
 	t_glx	*glx;
 
 	glx = ft_calloc(1, sizeof(t_glx));
 	glx->mlx = mlx_init();
+	glx->win = mlx_new_window(glx->mlx, win_w, win_h, title);
 	glx->run = glx_run;
 	glx->free = glx_free;
 	return (glx);
