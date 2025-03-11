c9bd8d2d1fe196c59b8497c1bee4432fe99cf58e
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-03-11 16:16:03 +0900

    add btnp

--- inc/glx.h
+++ inc/glx.h
@@ -6,18 +6,17 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 11:47:22 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/03/11 14:26:32 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/03/11 14:54:59 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GLX_H
 # define GLX_H
 
-# include <X11/keysym.h>
-# include <stdbool.h>
-
 # include "libft.h"
 # include "mlx.h"
+# include <X11/keysym.h>
+# include <stdbool.h>
 
 # define MAX_IMG 32
 # define KEY_MAX XK_Delete
@@ -28,11 +27,13 @@ typedef struct s_glx
 	void	*win;
 	void	*imgs[MAX_IMG];
 	int		imgc;
-	bool	key_state[KEY_MAX];
+	bool	prev_key_state[KEY_MAX];
+	bool	current_key_state[KEY_MAX];
 	void	(*load_img)(struct s_glx *, char *, int, int);
-	void	(*put_img)(struct s_glx*, int, int, int);
+	void	(*put_img)(struct s_glx *, int, int, int);
 	void	(*run)(struct s_glx *);
 	void	(*free)(struct s_glx *);
+	bool	(*btnp)(struct s_glx *, int);
 }			t_glx;
 
 t_glx		*glx_init(char *title, int win_w, int win_h);
@@ -40,5 +41,5 @@ void		glx_load_img(t_glx *self, char *path, int w, int h);
 void		glx_put_img(t_glx *self, int img_i, int w, int h);
 void		glx_run(t_glx *self);
 void		glx_free(t_glx *self);
-
+bool		glx_btnp(t_glx *self, int keycode);
 #endif
--- src/glx.c
+++ src/glx.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 11:46:33 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/03/11 14:22:30 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/03/11 16:15:32 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,11 @@ void	glx_key_state_init(t_glx *self)
 
 	i = 0;
 	while (i < KEY_MAX)
-		self->key_state[i++] = false;
+	{
+		self->prev_key_state[i] = false;
+		self->current_key_state[i] = false;
+		i++;
+	}
 }
 t_glx	*glx_init(char *title, int win_w, int win_h)
 {
@@ -32,26 +36,45 @@ t_glx	*glx_init(char *title, int win_w, int win_h)
 	glx->load_img = glx_load_img;
 	glx->put_img = glx_put_img;
 	glx->free = glx_free;
+	glx->btnp = glx_btnp;
 	glx_key_state_init(glx);
 	return (glx);
 }
 
+int _glx_update_key_state(t_glx *self)
+{
+	int i;
+
+	ft_memcpy(self->prev_key_state, self->current_key_state, sizeof(self->current_key_state));
+	i = 0;
+	while (i < KEY_MAX)
+		self->current_key_state[i] = false;
+	return (0);
+}
+
 int	_glx_key_pressed(int keycode, t_glx *glx)
 {
-	if (keycode > KEY_MAX)
+	if (keycode < 0 || keycode > KEY_MAX)
 		return (1);
-	glx->key_state[keycode] = true;
-	ft_printf("key_state[%d]= %d\n", keycode, glx->key_state[keycode]);
+	glx->current_key_state[keycode] = true;
 	return (0);
 }
 
 int	_glx_key_released(int keycode, t_glx *glx)
 {
-	if (keycode > KEY_MAX)
+	if (keycode < 0 || keycode > KEY_MAX)
 		return (1);
-	glx->key_state[keycode] = false;
-	ft_printf("key_state[%d]= %d\n", keycode, glx->key_state[keycode]);
+	glx->current_key_state[keycode] = false;
 	return (0);
+}
+
+bool glx_btnp(t_glx *self, int keycode)
+{
+	if (keycode < 0 || keycode > KEY_MAX)
+		return (false);
+	if (self->prev_key_state[keycode])
+		return (false);
+	return (self->current_key_state[keycode]);
 }
 
 void	glx_load_img(t_glx *self, char *path, int w, int h)
@@ -72,6 +95,7 @@ void	glx_free(t_glx *self)
 
 void	glx_run(t_glx *self)
 {
+	mlx_loop_hook(self->mlx, _glx_update_key_state, self);
 	mlx_hook(self->win, 2, 1L << 0, _glx_key_pressed, self);
 	mlx_hook(self->win, 3, 1L << 1, _glx_key_released, self);
 	mlx_loop(self->mlx);
