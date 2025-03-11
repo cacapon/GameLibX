d15ef596743e225d13be2b68b59dc0d28394e689
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-03-11 13:14:05 +0900

    add put_img

--- inc/glx.h
+++ inc/glx.h
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 11:47:22 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/03/11 12:52:07 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/03/11 13:08:01 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,24 @@
 # define GLX_H
 # include "libft.h"
 # include "mlx.h"
+# define MAX_IMG 32
 
 typedef struct s_glx
 {
 	void	*mlx;
 	void	*win;
+	void	*imgs[MAX_IMG];
+	int		imgc;
 	void	(*load_img)(struct s_glx *, char *, int, int);
+	void	(*put_img)(struct s_glx*, int, int, int);
 	void	(*run)(struct s_glx *);
 	void	(*free)(struct s_glx *);
 }			t_glx;
 
 t_glx		*glx_init(char *title, int win_w, int win_h);
 void		glx_load_img(t_glx *self, char *path, int w, int h);
-void		glx_free(t_glx *self);
+void		glx_put_img(t_glx *self, int img_i, int w, int h);
 void		glx_run(t_glx *self);
+void		glx_free(t_glx *self);
 
 #endif
--- src/glx.c
+++ src/glx.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 11:46:33 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/03/11 12:52:28 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/03/11 13:12:05 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,22 @@ t_glx	*glx_init(char *title, int win_w, int win_h)
 	glx = ft_calloc(1, sizeof(t_glx));
 	glx->mlx = mlx_init();
 	glx->win = mlx_new_window(glx->mlx, win_w, win_h, title);
+	glx->imgc = 0;
 	glx->run = glx_run;
 	glx->load_img = glx_load_img;
+	glx->put_img = glx_put_img;
 	glx->free = glx_free;
 	return (glx);
 }
 
 void	glx_load_img(t_glx *self, char *path, int w, int h)
 {
-	mlx_xpm_file_to_image(self->mlx, path, &w, &h);
+	self->imgs[self->imgc++] = mlx_xpm_file_to_image(self->mlx, path, &w, &h);
+}
+
+void	glx_put_img(t_glx *self, int img_i, int w, int h)
+{
+	mlx_put_image_to_window(self->mlx, self->win, self->imgs[img_i], w, h);
 }
 
 void	glx_free(t_glx *self)
