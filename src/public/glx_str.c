a2f674fa5edce5b6f1c69cec16d0ee2fea724dc9
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-03-17 14:11:58 +0900

    折返し担っている箇所を対応

--- src/public/glx_str.c
+++ src/public/glx_str.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 11:48:46 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/03/17 14:00:59 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/03/17 14:11:20 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,18 @@
 
 /**
  * @brief 文字列strを位置posにcolorの色で出力します。
- * 
- * @param self 
- * @param str 
- * @param pos 
- * @param color 
+ *
+ * @param self
+ * @param str
+ * @param pos
+ * @param color
  */
 void	glx_put_str(t_glx *self, char *str, t_pos pos, t_glx_color_i color)
 {
-	mlx_string_put(self->mlx, self->win, pos.x, pos.y, glx_get_color(color),
-		str);
+	void	*mlx;
+	void	*win;
+
+	mlx = self->mlx;
+	win = self->win;
+	mlx_string_put(mlx, win, pos.x, pos.y, glx_get_color(color), str);
 }
