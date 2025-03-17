15a89db1cc9addc8e9955ff94768d952af0b251f
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-03-17 12:55:00 +0900

    btnpにコメント追加

--- src/public/glx_key_input_pub.c
+++ src/public/glx_key_input_pub.c
@@ -6,12 +6,19 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 11:52:07 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/03/17 12:51:58 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/03/17 12:54:39 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "glx.h"
 
+/**
+ * @brief 指定したkeyが丁度押されたかチェックします。
+ * 
+ * @param self 
+ * @param keycode: X11ベースのキーコード 
+ * @return int: 丁度押された場合1を返します。それ以外は0。
+ */
 int	glx_btnp(t_glx *self, int keycode)
 {
 	if (keycode < 0 || keycode > KEY_MAX)
